5948e13e5bbeca2b97e506ec3ba2ddad34209c58
chanseok <chanseok@88015cbe-a304-4c29-8203-3a0fa617ae4c>
2014-10-06 19:51:51 +0000

    Fixing runconcrete.sh. Uses dsnlog.o.

--- cil-examples/runconcrete.sh
+++ cil-examples/runconcrete.sh
@@ -4,5 +4,6 @@ then echo "needs an argument";
     exit 1
 fi
 
-../cil-1.7.3/bin/cilly --dosimpleMem --dosimplify --domakeCFG --dodsnconcrete --save-temps "$1.linear.c"  
-./a.out > "$1.concrete.c"-lm
+../cil-1.7.3/bin/cilly --dosimpleMem --dosimplify --domakeCFG --dodsnconcrete --save-temps "$1.linear.c" dsnlog.o -lm
+./a.out
+mv dsn_logfile.txt "$1.concrete.c"
