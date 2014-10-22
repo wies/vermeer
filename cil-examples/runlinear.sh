4451d16df3834c88846d4ba27eb2a49bc8d7ff1f
dsn <dsn@88015cbe-a304-4c29-8203-3a0fa617ae4c>
2014-10-22 21:39:08 +0000

    automatically recreate dsn-log-o

--- cil-examples/runlinear.sh
+++ cil-examples/runlinear.sh
@@ -4,6 +4,10 @@ then echo "needs an argument";
     exit 1
 fi
 
+if [ ! -f dsnlog.o ]; then
+    . mk-log-o
+fi
+
 ../cil-1.7.3/bin/cilly --dosimpleMem --dosimplify --domakeCFG --dodsnlinear --save-temps "$1.c" dsnlog.o extern_debug_funs.o -lm
 
 if [ "$?" -ne "0" -o ! -x ./a.out ]; then exit 1; fi
