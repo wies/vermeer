b41d6a81b9cdeb1173bea2a42792357fd2bcb803
Daniel Schwartz-Narbonne <dsn@cs.nyu.edu>
2015-01-30 16:33:04 -0500

    runsmt modified to the new format

--- georg/runsmt.sh
+++ georg/runsmt.sh
@@ -4,7 +4,7 @@ then echo "needs an argument";
     exit 1
 fi
 
-$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread "$1.c" -lm
+$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=abstractenv "$1.c" -lm
 $VERMEER_PATH/georg/remap_variables "$1.c" smtresult.txt > "$1.result.txt"
 rm smtresult.txt
 rm *.o
