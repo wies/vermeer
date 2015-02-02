7a57a2f519b15d6a4b980201d2437da2c995bfcf
andreas-holzer <andreas.holzer81@gmail.com>
2015-02-02 12:32:13 -0500

    Script that prints SMT formula to stdout.

---
+++ georg/printsmt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$#" -ne 1 ] 
+then echo "needs an argument";
+    exit 1
+fi
+
+#$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=abstractenv "$1.c" -lm
+$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --smtmultithread=print_smt "$1.c" -lm
+#$VERMEER_PATH/georg/remap_variables "$1.c" smtresult.txt > "$1.result.txt"
+#rm smtresult.txt
+rm *.o
+
