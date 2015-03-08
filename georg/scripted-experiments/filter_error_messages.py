cdc16dc97c4279cdc904c5b92a93b3b910ca7b78
andreas-holzer <andreas.holzer81@gmail.com>
2015-03-08 15:57:20 -0400

    Script to filter output for error messages.

---
+++ georg/scripted-experiments/filter_error_messages.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+dat_file = sys.argv[1]
+out_file = sys.argv[2]
+
+with open(dat_file, "r") as f_dat:
+  for line in f_dat:
+    line_stripped = line.strip()
+    if not line_stripped.startswith("#"):
+      entries = line_stripped.split(",")
+      benchmark = entries[0]
+      trace_index = entries[1]
+      with open(out_file, "r") as f_out:
+        start_matching = False
+        start_printing = False
+        for line_out in f_out:
+          if start_matching:
+            if line_out.startswith("********** Processing directory ../"):
+              start_matching = False
+            else:
+              if start_printing:
+                if line_out.startswith("# process trace_assertion_failed_"):
+                  start_printing = False
+                else:
+                  sys.stdout.write(line_out)
+              else:
+                if line_out.strip() == "# process trace_assertion_failed_" + trace_index:
+                  start_printing = True
+                  sys.stdout.write(benchmark + "\n")
+                  sys.stdout.write(line_out)
+          else:
+            if line_out.startswith("********** Processing directory ../" + benchmark + "-traces **********"):
+              start_matching = True
+            else:
+              if line_out.startswith("********** Processing directory ../"):
+                start_matching = False
