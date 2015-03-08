486dbee8ecef60956a907f4f93b1c9db15aa0a47
andreas-holzer <andreas.holzer81@gmail.com>
2015-03-08 00:32:57 -0500

    Read option index from command line argument.

--- georg/scripted-experiments/check_data_file.py
+++ georg/scripted-experiments/check_data_file.py
@@ -4,8 +4,19 @@ import os
 comments = []
 faulty_entries = []
 entries = []
+index = sys.argv[1]
 
-with open("data_option1.dat", "r") as f:
+if len(sys.argv) <= 1:
+  sys.stdout.write("Usage: " + sys.argv[0] + " <n>\nwhere n is an option index\n")
+  exit(-1)
+else:
+  try:
+    option_index_value = int(sys.argv[1])
+  except ValueError:
+    sys.stdout.write("Usage: " + sys.argv[0] + " <n>\nwhere n is an option index\n")
+    exit(-1)
+
+with open("data_option" + str(option_index_value) + ".dat", "r") as f:
   for line in f:
     line_stripped = line.strip()
     if line_stripped.startswith("#"):
@@ -16,13 +27,13 @@ with open("data_option1.dat", "r") as f:
       else:
         entries.append(line)
 
-with open("data_option1.filtered.dat", "w") as pos_f:
+with open("data_option" + str(option_index_value) + ".filtered.dat", "w") as pos_f:
   for line in comments:
     pos_f.write(line)
   for line in entries:
     pos_f.write(line)
 
-with open("data_option1.faulty.dat", "w") as neg_f:
+with open("data_option" + str(option_index_value) + ".faulty.dat", "w") as neg_f:
   for line in comments:
     neg_f.write(line)
   for line in faulty_entries:
