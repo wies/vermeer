84c369d1da990e5e6f3ac7b53e170891eaf32914
Daniel Schwartz-Narbonne <dsn@cs.nyu.edu>
2015-03-16 17:17:50 -0400

    simplify

--- georg/traces/bankaccount/locks/simplify.sh
+++ georg/traces/bankaccount/locks/simplify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TRACE="trace_assertion_failed_5.c"
-ARGS="--smtmultithread=allthreads --flowsensitive --hazardsensitiveall --nointrathreadhazard" 
+ARGS="--smtmultithread=allthreads --flowsensitive --hazardsensitiveall --nointrathreadhazard --runsmtanalysistype=binarysearch " 
 BEAUTIFY="python $VERMEER_PATH/georg/scripted-experiments/beautify_explanation.py"
 REMAP="$VERMEER_PATH/georg/remap_variables"
-time $VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --smtcalcstats $ARGS $TRACE
+time $VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --smtcalcstats $ARGS $TRACE | tee out.txt
 
 $BEAUTIFY reduced.txt > reduced.beautify.txt;
 $BEAUTIFY threadSlice0.txt > threadSlice0.beautify.txt;
