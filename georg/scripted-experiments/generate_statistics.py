3d80bee4b3612428154ea9219712bb46ea66c2c3
andreas-holzer <andreas.holzer81@gmail.com>
2015-03-31 12:10:39 -0400

    Cleanup.

--- georg/scripted-experiments/generate_statistics.py
+++ georg/scripted-experiments/generate_statistics.py
@@ -102,29 +102,18 @@ options.append("--keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtm
 options.append("--keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=abstractenv --smtcalcstats --hazardsensitivewar --toposortinput --nointrathreadhazard")
 options.append("--keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=abstractenv --smtcalcstats --hazardsensitivewaw --toposortinput --nointrathreadhazard")
 
-if len(sys.argv) <= 1:
-  option_index_range = range(0, len(options))
-else:
-  try:
-    option_index_value = int(sys.argv[1])
-    option_index_range = [ option_index_value ]
-  except ValueError:
-    sys.stdout.write("Usage: " + sys.argv[0] + " <n>\n\nwhere n is an option index in the range [0," + str(len(options) - 1) + "] or is omitted:\n")
-    for i in range(0, len(options)):
-      sys.stdout.write("Option " + str(i) + ": " + options[i] + "\n")
-    sys.stdout.write("\n")
-    exit(-1)
+
 
 
 def process_directory(directory, cwd, data_set_list):
   sys.stdout.write("\n********** Processing directory " + directory + " **********\n\n")
   os.chdir(directory)
   for trace_file in glob.glob("trace_assertion_failed_*.c"):
-    process_trace(trace_file, options[option_index], data_set_list)
+    process_trace(trace_file, options[option_index], directory[10:-6], data_set_list)
   sys.stdout.write("\n")
   os.chdir(cwd)
 
-def process_trace(trace_file, configuration, data_set_list):
+def process_trace(trace_file, configuration, benchmark, data_set_list):
   name = trace_file[:-2]
   trace_index = trace_file[23:-2]
   sys.stdout.write("\n# process " + name + "\n\n")
@@ -142,7 +131,7 @@ def process_trace(trace_file, configuration, data_set_list):
         data_entry_list.append(DataEntry(entries[0], entries[2], entries[4], entries[6]))
     else:
       break
-  data_set_list.append(DataSet(directory[10:-6], trace_index, data_entry_list, (time_stop - time_start)))
+  data_set_list.append(DataSet(benchmark, trace_index, data_entry_list, (time_stop - time_start)))
   subprocess.call(["rm", "-f", "*.o"])
   cleanup_processes()
 
@@ -176,13 +165,27 @@ def write_data_set_to_file(data_set_list, data_file):
     data_file.write("," + str(data_set.duration))
     data_file.write("\n")
 
-for option_index in option_index_range:
-  sys.stdout.write("\n########## Processing option \"" + options[option_index] + "\" ##########\n\n")
-  with open("directories.txt", "r") as f:
-    data_set_list = []
-    for line in f:
-      directory = line.strip()
-      process_directory(directory, cwd, data_set_list)
-    data_file = open("./data/config-" + str(option_index) + "/data_option" + str(option_index) + ".dat", "w")
-    write_data_set_to_file(data_set_list, data_file)
+if __name__ == "__main__":
+  if len(sys.argv) <= 1:
+    option_index_range = range(0, len(options))
+  else:
+    try:
+      option_index_value = int(sys.argv[1])
+      option_index_range = [ option_index_value ]
+    except ValueError:
+      sys.stdout.write("Usage: " + sys.argv[0] + " <n>\n\nwhere n is an option index in the range [0," + str(len(options) - 1) + "] or is omitted:\n")
+      for i in range(0, len(options)):
+        sys.stdout.write("Option " + str(i) + ": " + options[i] + "\n")
+      sys.stdout.write("\n")
+      exit(-1)
+
+  for option_index in option_index_range:
+    sys.stdout.write("\n########## Processing option \"" + options[option_index] + "\" ##########\n\n")
+    with open("directories.txt", "r") as f:
+      data_set_list = []
+      for line in f:
+        directory = line.strip()
+        process_directory(directory, cwd, data_set_list)
+      data_file = open("./data/config-" + str(option_index) + "/data_option" + str(option_index) + ".dat", "w")
+      write_data_set_to_file(data_set_list, data_file)
 
--- georg/scripted-experiments/visualization/example.sh
+++ georg/scripted-experiments/visualization/example.sh
@@ -1,2 +1,2 @@
-python files_to_xml.py ~/vermeer/repo2/georg/traces/pool/locks/slice1_1.txt ~/vermeer/repo2/georg/traces/pool/locks/trace_assertion_failed_137.c > pool137.xml
+python files_to_xml.py ../slice1_1.txt ../../traces/pool/locks/trace_assertion_failed_137.c > pool137.xml
 python xml_to_html.py pool137.xml > pool137.html
