36c2237b9c4de6d65059cb07431dae326937e052
Daniel Schwartz-Narbonne <dsn@cs.nyu.edu>
2015-04-17 11:43:37 -0400

    no longer need to use the beautifer, since its integrated in

--- georg/traces/bankaccount/locks/simplify.sh
+++ georg/traces/bankaccount/locks/simplify.sh
@@ -5,13 +5,9 @@ BEAUTIFY="python $VERMEER_PATH/georg/scripted-experiments/beautify_explanation.p
 REMAP="$VERMEER_PATH/georg/remap_variables"
 time $VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --smtcalcstats --smtbeautify $ARGS $TRACE | tee out.txt
 
-$BEAUTIFY reduced.txt > reduced.beautify.txt;
-$BEAUTIFY threadSlice0.txt > threadSlice0.beautify.txt;
-$BEAUTIFY threadSlice1.txt > threadSlice1.beautify.txt;
-$BEAUTIFY threadSlice2.txt > threadSlice2.beautify.txt;
 
-$REMAP $TRACE reduced.beautify.txt > reduced.remap.txt;
-$REMAP $TRACE threadSlice0.beautify.txt > threadSlice0.remap.txt;
-$REMAP $TRACE threadSlice1.beautify.txt > threadSlice1.remap.txt;
-$REMAP $TRACE threadSlice2.beautify.txt > threadSlice2.remap.txt;
+$REMAP $TRACE reduced.txt > reduced.remap.txt;
+$REMAP $TRACE threadSlice0.txt > threadSlice0.remap.txt;
+$REMAP $TRACE threadSlice1.txt > threadSlice1.remap.txt;
+$REMAP $TRACE threadSlice2.txt > threadSlice2.remap.txt;
 
