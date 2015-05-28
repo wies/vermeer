a40da38b5caf57eb6b6301d65dac29d5b47aaa09
andreas-holzer <andreas.holzer81@gmail.com>
2015-05-28 14:50:06 -0400

    Activated formula beautification.

--- georg/scripted-experiments/icse16-user-study/runsmt.sh
+++ georg/scripted-experiments/icse16-user-study/runsmt.sh
@@ -4,6 +4,6 @@ then echo "needs an argument";
     exit 1
 fi
 
-$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=allthreads --flowsensitive --toposortinput --hazardsensitiveall --nointrathreadhazard "$1.c" -lm
+$VERMEER_PATH/cil-1.7.3/bin/cilly -c --keepunused --dodsnsmt --runsmtanalysistype=binarysearch --smtmultithread=allthreads --flowsensitive --toposortinput --hazardsensitiveall --nointrathreadhazard --smtbeautify "$1.c" -lm
 rm *.o
 
