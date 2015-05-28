727a8637cef1c87df6cf02fbab1d7de0891b95a8
andreas-holzer <andreas.holzer81@gmail.com>
2015-05-28 14:37:06 -0400

    Updated trace: trace that does not use numbugs anymore.

---
+++ georg/traces/jsinterp_kernel/locks/trace_assertion_failed_3.c
@@ -0,0 +1,307 @@
+#ifdef COMPILE_FOR_TEST
+#include <assert.h>
+#define assume(cond) assert(cond)
+#endif
+
+void main(int argc, char* argv[]) {
+int x_0_0;//CREST_scheduler::lock_0 
+int x_0_1;//CREST_scheduler::lock_0 
+int x_0_2;//CREST_scheduler::lock_0 
+int x_0_3;//CREST_scheduler::lock_0 
+int x_0_4;//CREST_scheduler::lock_0 
+int x_0_5;//CREST_scheduler::lock_0 
+int x_1_0;//preallocated_properties_index 
+int x_1_1;//preallocated_properties_index 
+int x_1_2;//preallocated_properties_index 
+int x_2_0;//preallocated_objectmaps_index 
+int x_2_1;//preallocated_objectmaps_index 
+int x_2_2;//preallocated_objectmaps_index 
+int x_3_0;//preallocated_objects_index 
+int x_3_1;//preallocated_objects_index 
+int x_3_2;//preallocated_objects_index 
+int x_4_0;//preallocated_cacheentries_index 
+int x_4_1;//preallocated_cacheentries_index 
+int x_5_0;//preallocated_properties[0].id 
+int x_5_1;//preallocated_properties[0].id 
+int x_6_0;//preallocated_objectmaps[0].nrefs 
+int x_6_1;//preallocated_objectmaps[0].nrefs 
+int x_7_0;//preallocated_objectmaps[0].nslots 
+int x_7_1;//preallocated_objectmaps[0].nslots 
+int x_8_0;//preallocated_objectmaps[0].freeslot 
+int x_8_1;//preallocated_objectmaps[0].freeslot 
+int x_9_0;//preallocated_properties[1].id 
+int x_10_0;//preallocated_objectmaps[1].nrefs 
+int x_11_0;//preallocated_objectmaps[1].nslots 
+int x_12_0;//preallocated_objectmaps[1].freeslot 
+int x_13_0;//preallocated_properties[2].id 
+int x_14_0;//preallocated_objectmaps[2].nrefs 
+int x_15_0;//preallocated_objectmaps[2].nslots 
+int x_16_0;//preallocated_objectmaps[2].freeslot 
+int x_17_0;//preallocated_properties[3].id 
+int x_18_0;//preallocated_objectmaps[3].nrefs 
+int x_19_0;//preallocated_objectmaps[3].nslots 
+int x_20_0;//preallocated_objectmaps[3].freeslot 
+int x_21_0;//preallocated_properties[4].id 
+int x_22_0;//preallocated_objectmaps[4].nrefs 
+int x_23_0;//preallocated_objectmaps[4].nslots 
+int x_24_0;//preallocated_objectmaps[4].freeslot 
+int x_25_0;//preallocated_properties[5].id 
+int x_26_0;//preallocated_objectmaps[5].nrefs 
+int x_27_0;//preallocated_objectmaps[5].nslots 
+int x_28_0;//preallocated_objectmaps[5].freeslot 
+int x_29_0;//preallocated_properties[6].id 
+int x_30_0;//preallocated_objectmaps[6].nrefs 
+int x_31_0;//preallocated_objectmaps[6].nslots 
+int x_32_0;//preallocated_objectmaps[6].freeslot 
+int x_33_0;//preallocated_properties[7].id 
+int x_34_0;//preallocated_objectmaps[7].nrefs 
+int x_35_0;//preallocated_objectmaps[7].nslots 
+int x_36_0;//preallocated_objectmaps[7].freeslot 
+int x_37_0;//preallocated_properties[8].id 
+int x_38_0;//preallocated_objectmaps[8].nrefs 
+int x_39_0;//preallocated_objectmaps[8].nslots 
+int x_40_0;//preallocated_objectmaps[8].freeslot 
+int x_41_0;//preallocated_properties[9].id 
+int x_42_0;//preallocated_objectmaps[9].nrefs 
+int x_43_0;//preallocated_objectmaps[9].nslots 
+int x_44_0;//preallocated_objectmaps[9].freeslot 
+int x_45_0;//preallocated_properties[10].id 
+int x_46_0;//preallocated_objectmaps[10].nrefs 
+int x_47_0;//preallocated_objectmaps[10].nslots 
+int x_48_0;//preallocated_objectmaps[10].freeslot 
+int x_49_0;//preallocated_properties[11].id 
+int x_50_0;//preallocated_objectmaps[11].nrefs 
+int x_51_0;//preallocated_objectmaps[11].nslots 
+int x_52_0;//preallocated_objectmaps[11].freeslot 
+int x_53_0;//preallocated_properties[12].id 
+int x_54_0;//preallocated_objectmaps[12].nrefs 
+int x_55_0;//preallocated_objectmaps[12].nslots 
+int x_56_0;//preallocated_objectmaps[12].freeslot 
+int x_57_0;//preallocated_properties[13].id 
+int x_58_0;//preallocated_objectmaps[13].nrefs 
+int x_59_0;//preallocated_objectmaps[13].nslots 
+int x_60_0;//preallocated_objectmaps[13].freeslot 
+int x_61_0;//preallocated_properties[14].id 
+int x_62_0;//preallocated_objectmaps[14].nrefs 
+int x_63_0;//preallocated_objectmaps[14].nslots 
+int x_64_0;//preallocated_objectmaps[14].freeslot 
+int x_65_0;//preallocated_properties[15].id 
+int x_66_0;//preallocated_objectmaps[15].nrefs 
+int x_67_0;//preallocated_objectmaps[15].nslots 
+int x_68_0;//preallocated_objectmaps[15].freeslot 
+int x_69_0;//preallocated_properties[16].id 
+int x_70_0;//preallocated_objectmaps[16].nrefs 
+int x_71_0;//preallocated_objectmaps[16].nslots 
+int x_72_0;//preallocated_objectmaps[16].freeslot 
+int x_73_0;//preallocated_properties[17].id 
+int x_74_0;//preallocated_objectmaps[17].nrefs 
+int x_75_0;//preallocated_objectmaps[17].nslots 
+int x_76_0;//preallocated_objectmaps[17].freeslot 
+int x_77_0;//preallocated_properties[18].id 
+int x_78_0;//preallocated_objectmaps[18].nrefs 
+int x_79_0;//preallocated_objectmaps[18].nslots 
+int x_80_0;//preallocated_objectmaps[18].freeslot 
+int x_81_0;//preallocated_properties[19].id 
+int x_82_0;//preallocated_objectmaps[19].nrefs 
+int x_83_0;//preallocated_objectmaps[19].nslots 
+int x_84_0;//preallocated_objectmaps[19].freeslot 
+int x_85_0;//cache_global.empty 
+int x_85_1;//cache_global.empty 
+int x_85_2;//cache_global.empty 
+int x_86_0;//cache_global.fills 
+int x_86_1;//cache_global.fills 
+int x_87_0;//cache_global.recycles 
+int x_87_1;//cache_global.recycles 
+int x_88_0;//cache_global.tests 
+int x_88_1;//cache_global.tests 
+int x_89_0;//cache_global.misses 
+int x_89_1;//cache_global.misses 
+int x_90_0;//cache_global.flushes 
+int x_90_1;//cache_global.flushes 
+int x_90_2;//cache_global.flushes 
+int x_91_0;//functioncall::param T0
+int x_91_1;//functioncall::param T0
+int x_91_2;//functioncall::param T0
+int x_91_3;//functioncall::param T0
+int x_91_4;//functioncall::param T0
+int x_91_5;//functioncall::param T0
+int x_91_6;//functioncall::param T0
+int x_91_7;//functioncall::param T0
+int x_91_8;//functioncall::param T0
+int x_91_9;//functioncall::param T0
+int x_91_10;//functioncall::param T0
+int x_91_11;//functioncall::param T0
+int x_91_12;//functioncall::param T0
+int x_91_13;//functioncall::param T0
+int x_91_14;//functioncall::param T0
+int x_91_15;//functioncall::param T0
+int x_91_16;//functioncall::param T0
+int x_91_17;//functioncall::param T0
+int x_91_18;//functioncall::param T0
+int x_91_19;//functioncall::param T0
+int x_91_20;//functioncall::param T0
+int x_91_21;//functioncall::param T0
+int x_92_0;//continuously_fill_cache::i T1
+int x_92_1;//continuously_fill_cache::i T1
+int x_93_0;//continuously_fill_cache::jso T1
+int x_94_0;//get_new_object::newmap T1
+int x_95_0;//get_new_object::newobj T1
+int x_96_0;//continuously_fill_cache::id T1
+int x_96_1;//continuously_fill_cache::id T1
+int x_97_0;//functioncall::param T1
+int x_97_1;//functioncall::param T1
+int x_98_0;//js_PropertyCacheFill::NUMFLUSHES_BEGIN T1
+int x_98_1;//js_PropertyCacheFill::NUMFLUSHES_BEGIN T1
+int x_98_2;//js_PropertyCacheFill::NUMFLUSHES_BEGIN T1
+
+T_0_0_0: x_0_0 = -1;
+T_0_1_0: x_1_0 = 0;
+T_0_2_0: x_1_1 = 0;
+T_0_3_0: x_2_0 = 0;
+T_0_4_0: x_2_1 = 0;
+T_0_5_0: x_3_0 = 0;
+T_0_6_0: x_3_1 = 0;
+T_0_7_0: x_4_0 = 0;
+T_0_8_0: x_4_1 = 0;
+T_0_9_0: x_91_0 = 1291903698;
+T_0_10_0: x_91_1 = 0;
+T_0_11_0: x_5_0 = 0;
+T_0_12_0: x_6_0 = 0;
+T_0_13_0: x_7_0 = 0;
+T_0_14_0: x_8_0 = 0;
+T_0_15_0: x_91_2 = 1 + x_91_1;
+T_0_16_0: x_9_0 = 0;
+T_0_17_0: x_10_0 = 0;
+T_0_18_0: x_11_0 = 0;
+T_0_19_0: x_12_0 = 0;
+T_0_20_0: x_91_3 = 1 + x_91_2;
+T_0_21_0: x_13_0 = 0;
+T_0_22_0: x_14_0 = 0;
+T_0_23_0: x_15_0 = 0;
+T_0_24_0: x_16_0 = 0;
+T_0_25_0: x_91_4 = 1 + x_91_3;
+T_0_26_0: x_17_0 = 0;
+T_0_27_0: x_18_0 = 0;
+T_0_28_0: x_19_0 = 0;
+T_0_29_0: x_20_0 = 0;
+T_0_30_0: x_91_5 = 1 + x_91_4;
+T_0_31_0: x_21_0 = 0;
+T_0_32_0: x_22_0 = 0;
+T_0_33_0: x_23_0 = 0;
+T_0_34_0: x_24_0 = 0;
+T_0_35_0: x_91_6 = 1 + x_91_5;
+T_0_36_0: x_25_0 = 0;
+T_0_37_0: x_26_0 = 0;
+T_0_38_0: x_27_0 = 0;
+T_0_39_0: x_28_0 = 0;
+T_0_40_0: x_91_7 = 1 + x_91_6;
+T_0_41_0: x_29_0 = 0;
+T_0_42_0: x_30_0 = 0;
+T_0_43_0: x_31_0 = 0;
+T_0_44_0: x_32_0 = 0;
+T_0_45_0: x_91_8 = 1 + x_91_7;
+T_0_46_0: x_33_0 = 0;
+T_0_47_0: x_34_0 = 0;
+T_0_48_0: x_35_0 = 0;
+T_0_49_0: x_36_0 = 0;
+T_0_50_0: x_91_9 = 1 + x_91_8;
+T_0_51_0: x_37_0 = 0;
+T_0_52_0: x_38_0 = 0;
+T_0_53_0: x_39_0 = 0;
+T_0_54_0: x_40_0 = 0;
+T_0_55_0: x_91_10 = 1 + x_91_9;
+T_0_56_0: x_41_0 = 0;
+T_0_57_0: x_42_0 = 0;
+T_0_58_0: x_43_0 = 0;
+T_0_59_0: x_44_0 = 0;
+T_0_60_0: x_91_11 = 1 + x_91_10;
+T_0_61_0: x_45_0 = 0;
+T_0_62_0: x_46_0 = 0;
+T_0_63_0: x_47_0 = 0;
+T_0_64_0: x_48_0 = 0;
+T_0_65_0: x_91_12 = 1 + x_91_11;
+T_0_66_0: x_49_0 = 0;
+T_0_67_0: x_50_0 = 0;
+T_0_68_0: x_51_0 = 0;
+T_0_69_0: x_52_0 = 0;
+T_0_70_0: x_91_13 = 1 + x_91_12;
+T_0_71_0: x_53_0 = 0;
+T_0_72_0: x_54_0 = 0;
+T_0_73_0: x_55_0 = 0;
+T_0_74_0: x_56_0 = 0;
+T_0_75_0: x_91_14 = 1 + x_91_13;
+T_0_76_0: x_57_0 = 0;
+T_0_77_0: x_58_0 = 0;
+T_0_78_0: x_59_0 = 0;
+T_0_79_0: x_60_0 = 0;
+T_0_80_0: x_91_15 = 1 + x_91_14;
+T_0_81_0: x_61_0 = 0;
+T_0_82_0: x_62_0 = 0;
+T_0_83_0: x_63_0 = 0;
+T_0_84_0: x_64_0 = 0;
+T_0_85_0: x_91_16 = 1 + x_91_15;
+T_0_86_0: x_65_0 = 0;
+T_0_87_0: x_66_0 = 0;
+T_0_88_0: x_67_0 = 0;
+T_0_89_0: x_68_0 = 0;
+T_0_90_0: x_91_17 = 1 + x_91_16;
+T_0_91_0: x_69_0 = 0;
+T_0_92_0: x_70_0 = 0;
+T_0_93_0: x_71_0 = 0;
+T_0_94_0: x_72_0 = 0;
+T_0_95_0: x_91_18 = 1 + x_91_17;
+T_0_96_0: x_73_0 = 0;
+T_0_97_0: x_74_0 = 0;
+T_0_98_0: x_75_0 = 0;
+T_0_99_0: x_76_0 = 0;
+T_0_100_0: x_91_19 = 1 + x_91_18;
+T_0_101_0: x_77_0 = 0;
+T_0_102_0: x_78_0 = 0;
+T_0_103_0: x_79_0 = 0;
+T_0_104_0: x_80_0 = 0;
+T_0_105_0: x_91_20 = 1 + x_91_19;
+T_0_106_0: x_81_0 = 0;
+T_0_107_0: x_82_0 = 0;
+T_0_108_0: x_83_0 = 0;
+T_0_109_0: x_84_0 = 0;
+T_0_110_0: x_91_21 = 1 + x_91_20;
+T_0_111_0: x_85_0 = 0;
+T_0_112_0: x_86_0 = 0;
+T_0_113_0: x_87_0 = 0;
+T_0_114_0: x_88_0 = 0;
+T_0_115_0: x_89_0 = 0;
+T_0_116_0: x_90_0 = 0;
+T_0_117_0: x_85_1 = 0;
+T_0_118_0: x_86_1 = 0;
+T_0_119_0: x_87_1 = 0;
+T_0_120_0: x_88_1 = 0;
+T_0_121_0: x_89_1 = 0;
+T_0_122_0: x_90_1 = 0;
+T_1_123_1: x_92_0 = 0;
+T_1_124_1: x_92_1 = 0;
+T_1_125_1: if (x_92_1 < 20)  x_93_0 = 0;
+T_1_126_1: if (x_92_1 < 20)  x_94_0 = 146;
+T_1_127_1: if (x_92_1 < 20)  x_2_2 = 1 + x_2_1;
+T_1_128_1: if (x_92_1 < 20)  x_6_1 = 0;
+T_1_129_1: if (x_92_1 < 20)  x_7_1 = 0;
+T_1_130_1: if (x_92_1 < 20)  x_8_1 = 0;
+T_1_131_1: if (x_92_1 < 20)  x_95_0 = -836716800;
+T_1_132_1: if (x_92_1 < 20)  x_3_2 = 1 + x_3_1;
+T_1_133_1: if (x_92_1 < 20)  x_1_2 = 1 + x_1_1;
+T_1_134_1: if (x_92_1 < 20)  x_5_1 = x_1_2;
+T_1_135_1: if (x_92_1 < 20)  x_96_0 = -836716800;
+T_1_136_1: if (x_92_1 < 20)  x_96_1 = x_92_1;
+T_1_137_1: if (x_92_1 < 20)  x_97_0 = 357914761;
+T_1_138_1: if (x_92_1 < 20)  x_97_1 = x_96_1;
+T_1_139_1: if (x_92_1 < 20)  x_98_0 = 11099;
+T_1_140_1: if (x_92_1 < 20)  x_98_1 = 0;
+T_1_141_1: if (x_92_1 < 20 && 0 == x_0_0 + 1)  x_0_1 = 1;
+T_1_142_1: if (x_92_1 < 20 && 1 == x_0_1)  x_98_2 = x_90_1;
+T_1_143_1: if (x_92_1 < 20 && 1 == x_0_1)  x_0_2 = -1;
+T_2_144_2: if (0 == x_0_2 + 1)  x_0_3 = 2;
+T_2_145_2: if (x_85_1 == 0 && 2 == x_0_3)  x_85_2 = 1;
+T_2_146_2: if (x_85_1 == 0 && 2 == x_0_3)  x_90_2 = 1 + x_90_1;
+T_2_147_2: if (2 == x_0_3)  x_0_4 = -1;
+T_1_148_1: if (x_92_1 < 20 && 0 == x_0_4 + 1)  x_0_5 = 1;
+T_1_149_1: if (x_92_1 < 20 && 1 == x_0_5)  assert(x_90_2 <= x_98_2);
+}
