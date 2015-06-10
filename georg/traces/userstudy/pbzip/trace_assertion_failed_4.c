4a0f450a6d2dd2faf7f72689e7093a22d979a789
andreas-holzer <andreas.holzer81@gmail.com>
2015-06-10 17:49:09 -0400

    Trace for pbzip user study.

---
+++ georg/traces/userstudy/pbzip/trace_assertion_failed_4.c
@@ -0,0 +1,277 @@
+#ifdef COMPILE_FOR_TEST
+#include <assert.h>
+#define assume(cond) assert(cond)
+#endif
+
+void main(int argc, char* argv[]) {
+int x_0_0;//output_done 
+int x_0_1;//output_done 
+int x_0_2;//output_done 
+int x_1_0;//fifo.empty 
+int x_1_1;//fifo.empty 
+int x_1_2;//fifo.empty 
+int x_2_0;//fifo.ready 
+int x_2_1;//fifo.ready 
+int x_3_0;//fifo.full 
+int x_3_1;//fifo.full 
+int x_3_2;//fifo.full 
+int x_4_0;//fifo.isFreed 
+int x_4_1;//fifo.isFreed 
+int x_4_2;//fifo.isFreed 
+int x_5_0;//CREST_scheduler::lock_0 
+int x_5_1;//CREST_scheduler::lock_0 
+int x_5_2;//CREST_scheduler::lock_0 
+int x_5_3;//CREST_scheduler::lock_0 
+int x_5_4;//CREST_scheduler::lock_0 
+int x_5_5;//CREST_scheduler::lock_0 
+int x_5_6;//CREST_scheduler::lock_0 
+int x_5_7;//CREST_scheduler::lock_0 
+int x_5_8;//CREST_scheduler::lock_0 
+int x_5_9;//CREST_scheduler::lock_0 
+int x_5_10;//CREST_scheduler::lock_0 
+int x_5_11;//CREST_scheduler::lock_0 
+int x_5_12;//CREST_scheduler::lock_0 
+int x_5_13;//CREST_scheduler::lock_0 
+int x_5_14;//CREST_scheduler::lock_0 
+int x_5_15;//CREST_scheduler::lock_0 
+int x_5_16;//CREST_scheduler::lock_0 
+int x_5_17;//CREST_scheduler::lock_0 
+int x_5_18;//CREST_scheduler::lock_0 
+int x_5_19;//CREST_scheduler::lock_0 
+int x_5_20;//CREST_scheduler::lock_0 
+int x_5_21;//CREST_scheduler::lock_0 
+int x_5_22;//CREST_scheduler::lock_0 
+int x_5_23;//CREST_scheduler::lock_0 
+int x_5_24;//CREST_scheduler::lock_0 
+int x_5_25;//CREST_scheduler::lock_0 
+int x_5_26;//CREST_scheduler::lock_0 
+int x_5_27;//CREST_scheduler::lock_0 
+int x_5_28;//CREST_scheduler::lock_0 
+int x_5_29;//CREST_scheduler::lock_0 
+int x_5_30;//CREST_scheduler::lock_0 
+int x_5_31;//CREST_scheduler::lock_0 
+int x_5_32;//CREST_scheduler::lock_0 
+int x_5_33;//CREST_scheduler::lock_0 
+int x_5_34;//CREST_scheduler::lock_0 
+int x_5_35;//CREST_scheduler::lock_0 
+int x_5_36;//CREST_scheduler::lock_0 
+int x_5_37;//CREST_scheduler::lock_0 
+int x_5_38;//CREST_scheduler::lock_0 
+int x_5_39;//CREST_scheduler::lock_0 
+int x_5_40;//CREST_scheduler::lock_0 
+int x_5_41;//CREST_scheduler::lock_0 
+int x_5_42;//CREST_scheduler::lock_0 
+int x_6_0;//allDone 
+int x_6_1;//allDone 
+int x_6_2;//allDone 
+int x_6_3;//allDone 
+int x_6_4;//allDone 
+int x_6_5;//allDone 
+int x_6_6;//allDone 
+int x_6_7;//allDone 
+int x_6_8;//allDone 
+int x_6_9;//allDone 
+int x_6_10;//allDone 
+int x_6_11;//allDone 
+int x_6_12;//allDone 
+int x_6_13;//allDone 
+int x_6_14;//allDone 
+int x_6_15;//allDone 
+int x_6_16;//allDone 
+int x_6_17;//allDone 
+int x_6_18;//allDone 
+int x_6_19;//allDone 
+int x_6_20;//allDone 
+int x_7_0;//produce::i T0
+int x_7_1;//produce::i T0
+int x_7_2;//produce::i T0
+int x_7_3;//produce::i T0
+int x_7_4;//produce::i T0
+int x_7_5;//produce::i T0
+int x_7_6;//produce::i T0
+int x_7_7;//produce::i T0
+int x_7_8;//produce::i T0
+int x_7_9;//produce::i T0
+int x_7_10;//produce::i T0
+int x_7_11;//produce::i T0
+int x_8_0;//functioncall::param T0
+int x_8_1;//functioncall::param T0
+int x_9_0;//functioncall::param T0
+int x_9_1;//functioncall::param T0
+int x_10_0;//functioncall::param T0
+int x_10_1;//functioncall::param T0
+int x_11_0;//functioncall::param T0
+int x_11_1;//functioncall::param T0
+int x_12_0;//functioncall::param T0
+int x_12_1;//functioncall::param T0
+int x_13_0;//functioncall::param T0
+int x_13_1;//functioncall::param T0
+int x_14_0;//functioncall::param T0
+int x_14_1;//functioncall::param T0
+int x_15_0;//functioncall::param T0
+int x_15_1;//functioncall::param T0
+int x_16_0;//functioncall::param T0
+int x_16_1;//functioncall::param T0
+int x_17_0;//functioncall::param T0
+int x_17_1;//functioncall::param T0
+int x_18_0;//consumer::i T1
+int x_18_1;//consumer::i T1
+int x_19_0;//output::i T2
+int x_19_1;//output::i T2
+int x_19_2;//output::i T2
+int x_19_3;//output::i T2
+int x_19_4;//output::i T2
+int x_19_5;//output::i T2
+int x_19_6;//output::i T2
+int x_19_7;//output::i T2
+int x_19_8;//output::i T2
+int x_19_9;//output::i T2
+int x_19_10;//output::i T2
+int x_19_11;//output::i T2
+int x_19_12;//output::i T2
+int x_19_13;//output::i T2
+int x_19_14;//output::i T2
+int x_19_15;//output::i T2
+int x_19_16;//output::i T2
+int x_19_17;//output::i T2
+int x_19_18;//output::i T2
+int x_19_19;//output::i T2
+int x_19_20;//output::i T2
+int x_19_21;//output::i T2
+
+T_0_0_0: x_0_0 = 0;
+T_0_1_0: x_0_1 = 0;
+T_0_2_0: x_1_0 = 0;
+T_0_3_0: x_2_0 = 0;
+T_0_4_0: x_3_0 = 0;
+T_0_5_0: x_4_0 = 0;
+T_0_6_0: x_1_1 = 1;
+T_0_7_0: x_2_1 = 0;
+T_0_8_0: x_3_1 = 0;
+T_0_9_0: x_4_1 = 0;
+T_0_10_0: x_5_0 = -1;
+T_0_11_0: x_6_0 = 0;
+T_0_12_0: if (0 == x_5_0 + 1)  x_5_1 = 0;
+T_0_13_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_0 = 0;
+T_0_14_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_1 = 0;
+T_0_15_0: if (x_1_1 != 0 && x_7_1 < 10 && 0 == x_5_1)  x_8_0 = 2044567533;
+T_0_16_0: if (x_1_1 != 0 && x_7_1 < 10 && 0 == x_5_1)  x_8_1 = 10;
+T_0_17_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_2 = 1 + x_7_1;
+T_0_18_0: if (x_1_1 != 0 && x_7_2 < 10 && 0 == x_5_1)  x_9_0 = 1872738683;
+T_0_19_0: if (x_1_1 != 0 && x_7_2 < 10 && 0 == x_5_1)  x_9_1 = 10;
+T_0_20_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_3 = 1 + x_7_2;
+T_0_21_0: if (x_1_1 != 0 && x_7_3 < 10 && 0 == x_5_1)  x_10_0 = 646169913;
+T_0_22_0: if (x_1_1 != 0 && x_7_3 < 10 && 0 == x_5_1)  x_10_1 = 10;
+T_0_23_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_4 = 1 + x_7_3;
+T_0_24_0: if (x_1_1 != 0 && x_7_4 < 10 && 0 == x_5_1)  x_11_0 = 5126479;
+T_0_25_0: if (x_1_1 != 0 && x_7_4 < 10 && 0 == x_5_1)  x_11_1 = 10;
+T_0_26_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_5 = 1 + x_7_4;
+T_0_27_0: if (x_1_1 != 0 && x_7_5 < 10 && 0 == x_5_1)  x_12_0 = 999598100;
+T_0_28_0: if (x_1_1 != 0 && x_7_5 < 10 && 0 == x_5_1)  x_12_1 = 10;
+T_0_29_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_6 = 1 + x_7_5;
+T_0_30_0: if (x_1_1 != 0 && x_7_6 < 10 && 0 == x_5_1)  x_13_0 = 1330312608;
+T_0_31_0: if (x_1_1 != 0 && x_7_6 < 10 && 0 == x_5_1)  x_13_1 = 10;
+T_0_32_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_7 = 1 + x_7_6;
+T_0_33_0: if (x_1_1 != 0 && x_7_7 < 10 && 0 == x_5_1)  x_14_0 = 681564223;
+T_0_34_0: if (x_1_1 != 0 && x_7_7 < 10 && 0 == x_5_1)  x_14_1 = 10;
+T_0_35_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_8 = 1 + x_7_7;
+T_0_36_0: if (x_1_1 != 0 && x_7_8 < 10 && 0 == x_5_1)  x_15_0 = 76567817;
+T_0_37_0: if (x_1_1 != 0 && x_7_8 < 10 && 0 == x_5_1)  x_15_1 = 10;
+T_0_38_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_9 = 1 + x_7_8;
+T_0_39_0: if (x_1_1 != 0 && x_7_9 < 10 && 0 == x_5_1)  x_16_0 = 1448303588;
+T_0_40_0: if (x_1_1 != 0 && x_7_9 < 10 && 0 == x_5_1)  x_16_1 = 10;
+T_0_41_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_10 = 1 + x_7_9;
+T_0_42_0: if (x_1_1 != 0 && x_7_10 < 10 && 0 == x_5_1)  x_17_0 = 610825537;
+T_0_43_0: if (x_1_1 != 0 && x_7_10 < 10 && 0 == x_5_1)  x_17_1 = 10;
+T_0_44_0: if (x_1_1 != 0 && 0 == x_5_1)  x_7_11 = 1 + x_7_10;
+T_0_45_0: if (x_1_1 != 0 && 0 == x_5_1)  x_3_2 = 1;
+T_0_46_0: if (x_1_1 != 0 && 0 == x_5_1)  x_1_2 = 0;
+T_0_47_0: if (0 == x_5_1)  x_5_2 = -1;
+T_2_48_2: x_19_0 = 0;
+T_2_49_2: x_19_1 = 0;
+T_2_50_2: if (x_19_1 < 20 && 0 == x_5_2 + 1)  x_5_3 = 2;
+T_2_51_2: if (x_19_1 < 20 && x_2_1 == 0 && 2 == x_5_3)  x_6_1 = x_6_0;
+T_2_52_2: if (x_19_1 < 20 && 2 == x_5_3)  x_5_4 = -1;
+T_2_53_2: x_19_2 = 1 + x_19_1;
+T_2_54_2: if (x_19_2 < 20 && 0 == x_5_4 + 1)  x_5_5 = 2;
+T_2_55_2: if (x_19_2 < 20 && x_2_1 == 0 && 2 == x_5_5)  x_6_2 = x_6_1;
+T_2_56_2: if (x_19_2 < 20 && 2 == x_5_5)  x_5_6 = -1;
+T_2_57_2: x_19_3 = 1 + x_19_2;
+T_2_58_2: if (x_19_3 < 20 && 0 == x_5_6 + 1)  x_5_7 = 2;
+T_2_59_2: if (x_19_3 < 20 && x_2_1 == 0 && 2 == x_5_7)  x_6_3 = x_6_2;
+T_2_60_2: if (x_19_3 < 20 && 2 == x_5_7)  x_5_8 = -1;
+T_2_61_2: x_19_4 = 1 + x_19_3;
+T_2_62_2: if (x_19_4 < 20 && 0 == x_5_8 + 1)  x_5_9 = 2;
+T_2_63_2: if (x_19_4 < 20 && x_2_1 == 0 && 2 == x_5_9)  x_6_4 = x_6_3;
+T_2_64_2: if (x_19_4 < 20 && 2 == x_5_9)  x_5_10 = -1;
+T_2_65_2: x_19_5 = 1 + x_19_4;
+T_2_66_2: if (x_19_5 < 20 && 0 == x_5_10 + 1)  x_5_11 = 2;
+T_2_67_2: if (x_19_5 < 20 && x_2_1 == 0 && 2 == x_5_11)  x_6_5 = x_6_4;
+T_2_68_2: if (x_19_5 < 20 && 2 == x_5_11)  x_5_12 = -1;
+T_2_69_2: x_19_6 = 1 + x_19_5;
+T_2_70_2: if (x_19_6 < 20 && 0 == x_5_12 + 1)  x_5_13 = 2;
+T_2_71_2: if (x_19_6 < 20 && x_2_1 == 0 && 2 == x_5_13)  x_6_6 = x_6_5;
+T_2_72_2: if (x_19_6 < 20 && 2 == x_5_13)  x_5_14 = -1;
+T_2_73_2: x_19_7 = 1 + x_19_6;
+T_2_74_2: if (x_19_7 < 20 && 0 == x_5_14 + 1)  x_5_15 = 2;
+T_2_75_2: if (x_19_7 < 20 && x_2_1 == 0 && 2 == x_5_15)  x_6_7 = x_6_6;
+T_2_76_2: if (x_19_7 < 20 && 2 == x_5_15)  x_5_16 = -1;
+T_2_77_2: x_19_8 = 1 + x_19_7;
+T_2_78_2: if (x_19_8 < 20 && 0 == x_5_16 + 1)  x_5_17 = 2;
+T_2_79_2: if (x_19_8 < 20 && x_2_1 == 0 && 2 == x_5_17)  x_6_8 = x_6_7;
+T_2_80_2: if (x_19_8 < 20 && 2 == x_5_17)  x_5_18 = -1;
+T_2_81_2: x_19_9 = 1 + x_19_8;
+T_2_82_2: if (x_19_9 < 20 && 0 == x_5_18 + 1)  x_5_19 = 2;
+T_2_83_2: if (x_19_9 < 20 && x_2_1 == 0 && 2 == x_5_19)  x_6_9 = x_6_8;
+T_2_84_2: if (x_19_9 < 20 && 2 == x_5_19)  x_5_20 = -1;
+T_2_85_2: x_19_10 = 1 + x_19_9;
+T_2_86_2: if (x_19_10 < 20 && 0 == x_5_20 + 1)  x_5_21 = 2;
+T_2_87_2: if (x_19_10 < 20 && x_2_1 == 0 && 2 == x_5_21)  x_6_10 = x_6_9;
+T_2_88_2: if (x_19_10 < 20 && 2 == x_5_21)  x_5_22 = -1;
+T_2_89_2: x_19_11 = 1 + x_19_10;
+T_2_90_2: if (x_19_11 < 20 && 0 == x_5_22 + 1)  x_5_23 = 2;
+T_2_91_2: if (x_19_11 < 20 && x_2_1 == 0 && 2 == x_5_23)  x_6_11 = x_6_10;
+T_2_92_2: if (x_19_11 < 20 && 2 == x_5_23)  x_5_24 = -1;
+T_2_93_2: x_19_12 = 1 + x_19_11;
+T_2_94_2: if (x_19_12 < 20 && 0 == x_5_24 + 1)  x_5_25 = 2;
+T_2_95_2: if (x_19_12 < 20 && x_2_1 == 0 && 2 == x_5_25)  x_6_12 = x_6_11;
+T_2_96_2: if (x_19_12 < 20 && 2 == x_5_25)  x_5_26 = -1;
+T_2_97_2: x_19_13 = 1 + x_19_12;
+T_2_98_2: if (x_19_13 < 20 && 0 == x_5_26 + 1)  x_5_27 = 2;
+T_2_99_2: if (x_19_13 < 20 && x_2_1 == 0 && 2 == x_5_27)  x_6_13 = x_6_12;
+T_2_100_2: if (x_19_13 < 20 && 2 == x_5_27)  x_5_28 = -1;
+T_2_101_2: x_19_14 = 1 + x_19_13;
+T_2_102_2: if (x_19_14 < 20 && 0 == x_5_28 + 1)  x_5_29 = 2;
+T_2_103_2: if (x_19_14 < 20 && x_2_1 == 0 && 2 == x_5_29)  x_6_14 = x_6_13;
+T_2_104_2: if (x_19_14 < 20 && 2 == x_5_29)  x_5_30 = -1;
+T_2_105_2: x_19_15 = 1 + x_19_14;
+T_2_106_2: if (x_19_15 < 20 && 0 == x_5_30 + 1)  x_5_31 = 2;
+T_2_107_2: if (x_19_15 < 20 && x_2_1 == 0 && 2 == x_5_31)  x_6_15 = x_6_14;
+T_2_108_2: if (x_19_15 < 20 && 2 == x_5_31)  x_5_32 = -1;
+T_2_109_2: x_19_16 = 1 + x_19_15;
+T_2_110_2: if (x_19_16 < 20 && 0 == x_5_32 + 1)  x_5_33 = 2;
+T_2_111_2: if (x_19_16 < 20 && x_2_1 == 0 && 2 == x_5_33)  x_6_16 = x_6_15;
+T_2_112_2: if (x_19_16 < 20 && 2 == x_5_33)  x_5_34 = -1;
+T_2_113_2: x_19_17 = 1 + x_19_16;
+T_2_114_2: if (x_19_17 < 20 && 0 == x_5_34 + 1)  x_5_35 = 2;
+T_2_115_2: if (x_19_17 < 20 && x_2_1 == 0 && 2 == x_5_35)  x_6_17 = x_6_16;
+T_2_116_2: if (x_19_17 < 20 && 2 == x_5_35)  x_5_36 = -1;
+T_2_117_2: x_19_18 = 1 + x_19_17;
+T_2_118_2: if (x_19_18 < 20 && 0 == x_5_36 + 1)  x_5_37 = 2;
+T_2_119_2: if (x_19_18 < 20 && x_2_1 == 0 && 2 == x_5_37)  x_6_18 = x_6_17;
+T_2_120_2: if (x_19_18 < 20 && 2 == x_5_37)  x_5_38 = -1;
+T_2_121_2: x_19_19 = 1 + x_19_18;
+T_2_122_2: if (x_19_19 < 20 && 0 == x_5_38 + 1)  x_5_39 = 2;
+T_2_123_2: if (x_19_19 < 20 && x_2_1 == 0 && 2 == x_5_39)  x_6_19 = x_6_18;
+T_2_124_2: if (x_19_19 < 20 && 2 == x_5_39)  x_5_40 = -1;
+T_2_125_2: x_19_20 = 1 + x_19_19;
+T_2_126_2: if (x_19_20 < 20 && 0 == x_5_40 + 1)  x_5_41 = 2;
+T_2_127_2: if (x_19_20 < 20 && x_2_1 == 0 && 2 == x_5_41)  x_6_20 = x_6_19;
+T_2_128_2: if (x_19_20 < 20 && 2 == x_5_41)  x_5_42 = -1;
+T_2_129_2: x_19_21 = 1 + x_19_20;
+T_2_130_2: x_0_2 = 1;
+T_0_131_0: if (x_0_2 != 0)  x_4_2 = 1;
+T_1_132_1: x_18_0 = 20205712;
+T_1_133_1: x_18_1 = 0;
+T_1_134_1: if (x_18_1 < 10)  assert(x_4_2 == 0);
+}
