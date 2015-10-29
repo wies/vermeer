c799ca4c8349160e6e36470d295ab1c756e77e88
andreas-holzer <andreas.holzer81@gmail.com>
2015-10-29 01:36:28 -0400

    Starting to process a file.

--- cil-1.7.3/src/merge/merge_main.ml
+++ cil-1.7.3/src/merge/merge_main.ml
@@ -1,12 +1,29 @@
 open Xml
 
+let handle_variable_declarations xml =
+  print_endline "TODO: handle_variable_declarations"
+
+let handle_statements xml = 
+  print_endline "TODO: handle_statements"
+
+let rec process_trace_children l = 
+  match l with 
+  | x :: m -> print_endline (Xml.tag x); process_trace_children m
+  | [] -> ()
+
+let handle_trace xml = 
+  if String.compare (Xml.tag xml) "trace" == 0 then
+    process_trace_children (Xml.children xml)
+  else
+    print_endline "ERROR: malformed xml file!"
+
 let run () = 
   (try
-    let x = (Xml.parse_string "<bla>hello</bla>")
+    let x = Xml.parse_file "example.xml" (*(Xml.parse_string "<bla>hello</bla>")*)
     in 
-      print_endline (Xml.to_string_fmt x)
+      handle_trace x
   with
-    | Xml.Error msg as e -> Printf.printf "Xml error: %s\n" (Xml.error msg)
+    | Xml.Error msg -> Printf.printf "Xml error: %s\n" (Xml.error msg)
   )
     
 let () = run()
