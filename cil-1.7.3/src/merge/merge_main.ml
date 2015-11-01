2becd5fe4eb76be084837e33814f5dee9b42190d
andreas-holzer <andreas.holzer81@gmail.com>
2015-10-31 20:17:48 -0400

    Simple extraction of variable declaration information.

--- cil-1.7.3/src/merge/merge_main.ml
+++ cil-1.7.3/src/merge/merge_main.ml
@@ -1,7 +1,10 @@
 open Xml
 
 let handle_variable_declarations xml =
-  print_endline "TODO: handle_variable_declarations"
+  let aux decls xml_child = (Xml.attrib xml_child "id") :: decls in
+  let var_decls = List.rev (Xml.fold aux [] xml) in
+  let aux_two element = print_endline element in
+  List.iter aux_two var_decls
 
 let handle_statements xml = 
   print_endline "TODO: handle_statements"
