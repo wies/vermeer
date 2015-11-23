db311646f051cc17e90114e4bb96daba12cc86e8
andreas-holzer <andreas.holzer81@gmail.com>
2015-11-23 17:07:52 -0500

    Types for trace data structure.

--- georg/merger/merger.cpp
+++ georg/merger/merger.cpp
@@ -4,8 +4,64 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #include "rapidxml-1.13/rapidxml.hpp"
+
+enum variable_types_t {
+  INT
+};
+
+struct variable_declaration_t {
+  int id;
+  int variable;
+  int ssa_index;
+  variable_types_t type;
+  int thread;
+};
+
+struct variable_declarations_t {
+  std::vector<variable_declaration_t> decls;
+};
+
+struct multiplication_t {
+  int variable_id;
+  int factor;
+};
+
+struct term_t {
+  std::vector<multiplication_t> mults;
+  int constant;
+};
+
+enum ops {
+  EQ, NEQ, LT, LEQ, GT, GEQ
+};
+
+struct expression_t {
+  ops op;
+  term_t term;
+};
+
+struct guard_t {
+  std::vector<expression_t> exprs;
+};
+
+enum statement_type_t {
+  ASSIGNMENT, ASSERTION, ASSUMPTION
+};
+
+struct statement_t {
+  statement_type_t type;
+  int variable_id;
+  term_t rhs;
+  guard_t guard;
+};
+
+struct trace_t {
+  std::vector<variable_declaration_t> variable_declarations;
+  std::vector<statement_t> statements;
+};
 
 char* read_document(const std::string& filename) {
 
@@ -44,10 +100,16 @@ int main(int argc, char* argv[]) {
 
   std::cout << doc.first_node()->name() << std::endl;
 
-  rapidxml::xml_node<char>* n = doc.first_node()->first_node("declarations");
+  rapidxml::xml_node<char>* n_declarations = doc.first_node()->first_node("declarations");
 
-  if (n) {
-    std::cout << n->name() << std::endl;
+  if (n_declarations) {
+    std::cout << n_declarations->name() << std::endl;
+  }
+
+  rapidxml::xml_node<char>* n_statements = doc.first_node()->first_node("statements");
+
+  if (n_statements) {
+    std::cout << n_statements->name() << std::endl;
   }
 
   delete[] document_string;
