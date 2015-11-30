10c4840e0c898bd6172040fe27abc81708cbc4af
andreas-holzer <andreas.holzer81@gmail.com>
2015-11-30 18:43:40 -0500

    Visitor pattern for local executions.

--- georg/merger/alphabet.h
+++ georg/merger/alphabet.h
@@ -11,17 +11,55 @@ struct ssa_variable_t {
   int ssa_index;
 };
 
-enum stmt_type_t {
-  PI_ASSIGNMENT, /*PHI_ASSIGNMENT,*/ LOCAL_ASSIGNMENT
+struct pi_assignment_t;
+struct local_assignment_t;
+struct phi_assignment_t;
+
+struct stmt_visitor_t {
+
+  virtual void visit_pi_assignment(pi_assignment_t& a) = 0;
+  virtual void visit_local_assignment(local_assignment_t& a) = 0;
+  virtual void visit_phi_assignment(phi_assignment_t& a) = 0;
+
 };
 
-struct pi_assignment_t {
+struct stmt_t {
+
+  virtual void accept(stmt_visitor_t& visitor) = 0;
+
+};
+
+struct pi_assignment_t : public stmt_t {
   ssa_variable_t variable;
+
+  void accept(stmt_visitor_t& visitor) override {
+    visitor.visit_pi_assignment(*this);
+  }
+
 };
 
-struct local_assignment_t {
+struct local_assignment_t : public stmt_t {
   ssa_variable_t variable;
   /* ... */
+
+  void accept(stmt_visitor_t& visitor) override {
+    visitor.visit_local_assignment(*this);
+  }
+
+};
+
+struct phi_assignment_t : public stmt_t {
+  ssa_variable_t variable;
+  /* ... */
+
+  void accept(stmt_visitor_t& visitor) override {
+    visitor.visit_phi_assignment(*this);
+  }
+
+};
+
+struct local_execution_t {
+  std::vector<stmt_t*> stmts;
 };
 
 }
