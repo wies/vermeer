92d08c547e404098de1d46f094b54ea1343b0ef9
andreas-holzer <andreas.holzer81@gmail.com>
2015-12-01 11:41:13 -0500

    Refactoring: better identification of ssa-index components.

--- georg/merger/alphabet.h
+++ georg/merger/alphabet.h
@@ -4,15 +4,22 @@
 namespace alphabet {
 
 struct ssa_variable_t {
+
   int unique_id;
   int variable_id;
+
   /* type, currently: INT */
-  int thread_id;
-  int ssa_index;
+
+  struct ssa_index_t {
+    int thread_id;
+    int thread_local_index;
+  } ssa_index;
+
 };
 
 struct pi_assignment_t;
 struct local_assignment_t;
+struct global_assignment_t;
 struct phi_assignment_t;
 struct local_execution_t;
 
@@ -20,6 +27,7 @@ struct stmt_visitor_t {
 
   virtual void visit_pi_assignment(pi_assignment_t& a) = 0;
   virtual void visit_local_assignment(local_assignment_t& a) = 0;
+  virtual void visit_global_assignment(global_assignment_t& a) = 0;
   virtual void visit_phi_assignment(phi_assignment_t& a) = 0;
   virtual void visit_local_execution(local_execution_t& e) = 0;
 
@@ -32,7 +40,8 @@ struct stmt_t {
 };
 
 struct pi_assignment_t : public stmt_t {
-  ssa_variable_t variable;
+  ssa_variable_t global_variable; // lhs
+  // TODO how do we connect data flows with this assignment?
 
   void accept(stmt_visitor_t& visitor) override {
     visitor.visit_pi_assignment(*this);
@@ -41,8 +50,8 @@ struct pi_assignment_t : public stmt_t {
 };
 
 struct local_assignment_t : public stmt_t {
-  ssa_variable_t variable;
-  /* ... */
+  ssa_variable_t local_variable; // lhs
+  /* ... */ // rhs consists only of local variables or constants
 
   void accept(stmt_visitor_t& visitor) override {
     visitor.visit_local_assignment(*this);
@@ -50,8 +59,18 @@ struct local_assignment_t : public stmt_t {
 
 };
 
+struct global_assignment_t : public stmt_t {
+  ssa_variable_t local_variable; // lhs
+  ssa_variable_t shared_variable; // rhs
+
+  void accept(stmt_visitor_t& visitor) override {
+    visitor.visit_global_assignment(*this);
+  }
+
+};
+
 struct phi_assignment_t : public stmt_t {
-  ssa_variable_t variable;
+  ssa_variable_t variable; // lhs
   /* ... */
 
   void accept(stmt_visitor_t& visitor) override {
