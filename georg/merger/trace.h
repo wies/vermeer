1cca7d8d6e942f4136bd53bb110a07483303e8b2
andreas-holzer <andreas.holzer81@gmail.com>
2015-11-30 23:29:41 -0500

    Alternative statement structure.

--- georg/merger/trace.h
+++ georg/merger/trace.h
@@ -5,6 +5,20 @@
 
 namespace exe {
 
+struct execution_t;
+struct assignment_t;
+struct assertion_t;
+struct assumption_t;
+
+struct stmt_visitor_t {
+
+  virtual void visit_execution(execution_t& e) = 0;
+  virtual void visit_assignment(assignment_t& a) = 0;
+  virtual void visit_assertion(assertion_t& a) = 0;
+  virtual void visit_assumption(assumption_t& a) = 0;
+
+};
+
 enum variable_types_t {
   INT
 };
@@ -52,6 +66,55 @@ struct statement_t {
   int position;
   int thread;
   std::vector<expression_t> exprs;
+};
+
+struct stmt_t {
+
+  virtual ~stmt_t() {}
+
+  guard_t guard;
+  int position;
+  int thread;
+
+  virtual void accept(stmt_visitor_t& v) = 0;
+
+};
+
+struct assignment_t : public stmt_t {
+
+  virtual ~assignment_t() {}
+
+  int variable_id;
+  term_t rhs;
+
+  virtual void accept(stmt_visitor_t& v) override {
+    v.visit_assignment(*this);
+  }
+
+};
+
+struct assertion_t : public stmt_t {
+
+  virtual ~assertion_t() {}
+
+  std::vector<expression_t> exprs;
+
+  virtual void accept(stmt_visitor_t& v) override {
+    v.visit_assertion(*this);
+  }
+
+};
+
+struct assumption_t : public stmt_t {
+
+  virtual ~assumption_t() {}
+
+  std::vector<expression_t> exprs;
+
+  virtual void accept(stmt_visitor_t& v) override {
+    v.visit_assumption(*this);
+  }
+
 };
 
 // TODO we have to make sure that variable_declarations and statements are ordered according to their id and position, respectively. We don't need vectors, we can preallocate arrays.
