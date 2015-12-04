76ad80f32c0589ca3073a71c4aaf692ac8252e0a
andreas-holzer <andreas.holzer81@gmail.com>
2015-12-04 10:45:51 -0500

    Cleanup.

--- georg/merger/merger.cpp
+++ georg/merger/merger.cpp
@@ -47,87 +47,6 @@ struct thread_local_position_t {
   }
 };
 
-#if 0
-std::set<int> extract_variables(const exe::expression_t& e) {
-  std::set<int> variable_ids;
-
-  for (const exe::linear_product_t& p : e.term.products) {
-    variable_ids.insert(p.variable_id);
-  }
-
-  return variable_ids;
-}
-#endif
-
-#if 0
-std::vector<thread_local_position_t> extract_thread_local_positions(const exe::execution_t& e) {
-  std::vector<thread_local_position_t> v;
-
-  // thread_id x thread-local position
-  std::map<int, int> thread_local_counters;
-
-  // variable_id x global position
-  std::map<int, int> variable_definitions;
-
-  // variable_id x set of global positions
-  std::map<int, std::set<int> > variable_uses;
-
-  // TODO do we assume that statements are ordered according to their position attribute?
-  for (auto const& s : e.statements) {
-    int pos = thread_local_counters[s->thread];
-    v.push_back({ s->thread, pos });
-    thread_local_counters[s->thread] = pos + 1;
-
-    // track variable definition
-    if (s->type == exe::ASSIGNMENT) {
-      variable_definitions[s->variable_id] = s->position;
-      std::cout << "Variable " << s->variable_id << " is defined at " << v[variable_definitions[s->variable_id]] << std::endl;
-    }
-
-    // track variable usage
-    // a) guards
-    std::set<int> variable_ids;
-    for (auto const& e : s->guard.exprs) {
-      auto var_ids = extract_variables(e);
-      variable_ids.insert(var_ids.begin(), var_ids.end());
-    }
-
-    // b) variables in other expressions
-    switch (s->type) {
-      case exe::ASSIGNMENT:
-        for (auto const& p : s->rhs.products) {
-          if (e.variable_declarations[p.variable_id].thread < 0) { // global variable
-            std::cout << "G" << p.variable_id;
-          }
-          variable_ids.insert(p.variable_id);
-        }
-        break;
-      case exe::ASSERTION:
-      case exe::ASSUMPTION:
-        for (auto const& e : s->exprs) {
-          auto var_ids = extract_variables(e);
-          variable_ids.insert(var_ids.begin(), var_ids.end());
-        }
-        break;
-    }
-
-    for (int var_id : variable_ids) {
-      if (e.variable_declarations[var_id].thread < 0) { // global variable
-        std::cout << "g";
-      }
-      std::cout << var_id << " ";
-    }
-    std::cout << std::endl;
-  }
-
-  for (auto const& p : variable_uses) {
-    std::cout << p.first << std::endl;
-  }
-
-  return v;
-}
-#endif
-
 struct local_execution_extractor_t : public exe::stmt_visitor_t {
 
   std::map<int, std::vector<alphabet::stmt_t*>> local_executions;
@@ -314,14 +233,6 @@ int main(int argc, char* argv[]) {
   for (auto& p : lee.local_executions) {
     std::cout << "Thread " << p.first << ": " << p.second.size() << std::endl;
   }
-
-#if 0
-  auto pos = extract_thread_local_positions(e);
-
-  for (auto const& p : pos) {
-    std::cout << p << std::endl;
-  }
-#endif
 
   return EXIT_SUCCESS;
 }
