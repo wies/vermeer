8b5ca5b6fd2174419edff361c5b67aab1f8b15eb
andreas-holzer <andreas.holzer81@gmail.com>
2015-12-10 17:21:31 -0500

    Stub for phi assignments.

--- georg/merger/alphabet.h
+++ georg/merger/alphabet.h
@@ -206,6 +206,21 @@ struct pi_assignment_t : public stmt_t {
 
 };
 
+struct pi_assignments_t : public stmt_t {
+
+  // TODO fix the type!!!
+  pi_assignments_t() : stmt_t(PI_ASSIGNMENT) {}
+
+  void accept(stmt_visitor_t& visitor) override {
+    ERROR("Unsupported operation");
+  }
+
+  void print(std::ostream& out) const override {
+    ERROR("Unsupported operation");
+  }
+
+};
+
 struct global_assignment_t : public stmt_t {
   ssa_variable_t shared_variable; // lhs
   expr::term_t<ssa_variable_t> rhs;
